64fe2ea8ce2046ba4c2f27bced7670404620f8be
SimoneLollini <lollinisimone@gmail.com>
2023-03-17 14:23:52 +0100

    end milestone 2

--- src/org/eventi/java/Evento.java
+++ src/org/eventi/java/Evento.java
@@ -3,23 +3,20 @@ package org.eventi.java;
 import java.math.BigDecimal;
 import java.time.DateTimeException;
 import java.time.LocalDate;
-import java.util.Date;
 
 public class Evento {
 
-    String title;
-    LocalDate date;
-
-    BigDecimal totalSeats;
-
-    BigDecimal bookedSeats = new BigDecimal(0);
+    private String title;
+    private LocalDate date;
+    private BigDecimal totalSeats;
+    private BigDecimal bookedSeats = new BigDecimal(0);
 
     public Evento(String title, LocalDate date, BigDecimal totalSeats) throws DateTimeException, IllegalSeatsNumber {
 
-        if (date.isBefore(LocalDate.now())){
+        if (date.isBefore(LocalDate.now())) {
             throw new DateTimeException("Non puoi inserire una data passata!");
         }
-        if (totalSeats.compareTo(BigDecimal.ZERO) < 0){
+        if (totalSeats.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalSeatsNumber("I posti totali devono essere superiori o uguali ad 1");
         }
 
@@ -54,23 +51,22 @@ public class Evento {
     }
 
 
-    public void reserve(){
-        if (date.isBefore(LocalDate.now())){
+    public void reserve() {
+        if (date.isBefore(LocalDate.now())) {
             throw new DateTimeException("Non puoi prenotarti ad un evento già concluso!");
         }
-        if (bookedSeats.add(BigDecimal.ONE).compareTo(totalSeats) > 0){
+        if (bookedSeats.add(BigDecimal.ONE).compareTo(totalSeats) > 0) {
             throw new NoSeatsException("Mi dispiace non ci sono più posti disponibili.");
         }
         bookedSeats = bookedSeats.add(BigDecimal.valueOf(1));
     }
 
 
-
-    public void removeReservation(){
-        if (date.isBefore(LocalDate.now())){
+    public void removeReservation() {
+        if (date.isBefore(LocalDate.now())) {
             throw new DateTimeException("Non puoi rimuovere la prenozione ad un evento già concluso!");
         }
-        if (bookedSeats.subtract(BigDecimal.ONE).compareTo(BigDecimal.ZERO)<0){
+        if (bookedSeats.subtract(BigDecimal.ONE).compareTo(BigDecimal.ZERO) < 0) {
             throw new NoSeatsException("Mi dispiace non ci sono altre prenotazioni da rimuovere.");
         }
         bookedSeats = bookedSeats.subtract(BigDecimal.ONE);
--- src/org/eventi/java/Main.java
+++ src/org/eventi/java/Main.java
@@ -1,18 +1,10 @@
 package org.eventi.java;
 
+import java.math.BigDecimal;
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.util.Scanner;
-//        1. Creare una classe Main di test, in cui si chiede all’utente di inserire un nuovo evento
-//        con tutti i parametri.
 
-//        2. Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni
-//        vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo
-//        eventuali eccezioni.
-//
-//        3. Stampare a video il numero di posti prenotati e quelli disponibili
-//        4. Chiedere all’utente se e quanti posti vuole disdire
-//        5. Provare ad effettuare le disdette, implementando opportuni controlli e gestendo
-//        eventuali eccezioni
-//        6. Stampare a video il numero di posti prenotati e quelli disponibili
 public class Main {
 
     public static void main(String[] args) {
@@ -22,42 +14,78 @@ public class Main {
         String title;
         String date;
         int totalSeats;
-
+        int reservationNum;
 
         boolean emptyFields = true; // cicli while
-        boolean newReservation = false; // richiesta nuova prenotazione
+        boolean newChoise = false; // richiesta all'utente di fare un operazione (es. aggiungere e rimuovere prenotazioni)
 
-//        while (emptyFields) {
-//            System.out.println("Inserisci titolo evento: ");
-//            title = scan.nextLine();
-//            System.out.println("Inserisci data evento: (0000-00-00)");
-//            date = scan.nextLine();
-//            System.out.println("Inserisci i posti a sedere massimi dell evento: ");
-//            totalSeats = Integer.parseInt(scan.nextLine());
-//
-//            try {
-//                evento = new Evento(title, LocalDate.parse(date), new BigDecimal(totalSeats));
-//            } catch (NoSeatsException | IllegalSeatsNumber | DateTimeException e) {
-//                System.out.println(e.getMessage());
-//            }
-//            if (evento != null) {
-//                System.out.println("Evento inserito correttamente!");
-//                emptyFields = false;
-//            }
-//        }
+        while (emptyFields) {
+            System.out.println("Inserisci titolo evento: ");
+            title = scan.nextLine();
+            System.out.println("Inserisci data evento: (0000-00-00)");
+            date = scan.nextLine();
+            System.out.println("Inserisci i posti a sedere massimi dell evento: ");
+            totalSeats = Integer.parseInt(scan.nextLine());
 
-        //        2. Dopo che l’evento è stato istanziato, chiedere all’utente se e quante prenotazioni
-        //        vuole fare e provare ad effettuarle, implementando opportuni controlli e gestendo
-        //        eventuali eccezioni.
+            try {
+                evento = new Evento(title, LocalDate.parse(date), new BigDecimal(totalSeats));
+            } catch (NoSeatsException | IllegalSeatsNumber | DateTimeException e) {
+                System.out.println(e.getMessage());
+            }
+
+            if (evento != null) {
+                System.out.println("Evento inserito correttamente!");
+                emptyFields = false;
+            }
+        } //end while loop
 
         System.out.println("Vuoi inserire delle prenotazioni? (Y/N)");
-        newReservation = scan.nextLine().equalsIgnoreCase("Y");
+        newChoise = scan.nextLine().equalsIgnoreCase("Y");
 
-        if (newReservation) {
-            //inserire nuove prenotazioni
-        }
+        if (newChoise) {
+            System.out.println("Quante prenotazioni vuoi inserire?");
+            reservationNum = Integer.parseInt(scan.nextLine());
+
+            for (int i = 0; i < reservationNum; i++) {
+                try {
+                    evento.reserve();
+                } catch (NoSeatsException e) {
+                    System.out.println(e.getMessage());
+                    break;
+                } catch (DateTimeException e) {
+                    System.out.println(e.getMessage()); //valutare di togliere
+                }
+            } //end for loop
+        } //end if
 
 
+        System.out.println("Numero posti prenotati: " + evento.getBookedSeats() + "\n" +
+                "Posti ancora disponibili: " + evento.getTotalSeats().subtract(evento.getBookedSeats())
+        );
+
+
+        System.out.println("Vuoi rimuovere delle prenotazioni? (Y/N)");
+        newChoise = scan.nextLine().equalsIgnoreCase("Y");
+
+        if (newChoise) {
+            System.out.println("Quante prenotazioni vuoi eliminare?");
+            reservationNum = Integer.parseInt(scan.nextLine());
+
+            for (int i = 0; i < reservationNum; i++) {
+                try {
+                    evento.removeReservation();
+                } catch (NoSeatsException e) {
+                    System.out.println(e.getMessage());
+                    break;
+                } catch (DateTimeException e) {
+                    System.out.println(e.getMessage()); //valutare di togliere
+                }
+            } //end for loop
+        }   //end if
+
+        System.out.println("Numero posti prenotati: " + evento.getBookedSeats() + "\n" +
+                "Posti ancora disponibili: " + evento.getTotalSeats().subtract(evento.getBookedSeats())
+        );
     }
-
 }
+
